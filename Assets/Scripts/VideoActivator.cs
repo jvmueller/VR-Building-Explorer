492fa585d9a1f7e3b8e8daf0563060123b880a94
jvmueller <jvmuelle@umich.edu>
2025-06-09 14:04:33 -0400

    started conversion to skybox
    started converting from video sphere to skybox material switching. so far, there is still unintended behavior.

--- Assets/Scripts/VideoActivator.cs
+++ Assets/Scripts/VideoActivator.cs
@@ -1,97 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class VideoActivator : MonoBehaviour {
-    [SerializeField] private GameObject videoSphere;
+
+    public GameObject[] objectsInScene;
+    //public FadeCanvas fadeCanvas;
     public VideoPlayer videoPlayer;
+    public Material videoMaterial;
     public Transform playerTransform;
-    [SerializeField] private Vector3 startPosition = new Vector3(0.5f,1,25f);
-    //public GameObject building;
-    [SerializeField] private Material videoMaterial;
     [SerializeField] private float fadeDuration = 1f;
-    private bool _isActive;
+
+    private Material _skyboxMaterial;
 
     void Start(){
-        _isActive = false;
-        videoMaterial.SetFloat("_Alpha", 0);
-        //videoSphere.SetActive(false);
-        videoPlayer.Stop();
-        SetMovementActive(true);
+        _skyboxMaterial = RenderSettings.skybox;
     }
 
-    public void ToggleVideo(){
-        if (videoSphere.activeSelf){
-            DeactivateVideo();
-        }else{
-            ActivateVideo();
-        }
-    }
-
-    public void ActivateVideo(){
-        //building.SetActive(false);
-        //videoMaterial.SetFloat("_Alpha", 0);
-        //videoSphere.SetActive(true);
-        if (_isActive){
-            StartCoroutine(WaitForFade(!_isActive));
-        }
-        StartCoroutine(FadeCoroutine(0, 1, true));
-        videoPlayer.Play();
-        
-
-        //transform.position = playerTransform.position + new Vector3(0,1,1f);
-            videoSphere.transform.position = playerTransform.position + new Vector3(0, 1f, 0);
-            SetMovementActive(false);
-        
-
+    public void ActivateVideo() {
+        StartCoroutine(FadeAndSwitchVideo(videoMaterial, videoPlayer.Play));
+        SetMovementActive(false);
     }
 
     public void DeactivateVideo(){
-        
-        if (!_isActive) {
-            StartCoroutine(WaitForFade(_isActive));
-        }
-
-        
-        //building.SetActive(true);
-        StartCoroutine(FadeCoroutine(1, 0, false));
-        
-
-        //videoSphere.SetActive(false);
-            //videoPlayer.Stop();
-            //videoSphere.transform.position = startPosition;
-            SetMovementActive(true);
-        
+        StartCoroutine(FadeAndSwitchVideo(_skyboxMaterial, videoPlayer.Stop));
+        SetMovementActive(true);
     }
 
+    private IEnumerator FadeAndSwitchVideo(Material targetMaterial, Action onCompleteAction){
+        //fadeCanvas.QuickFadeIn();
+        yield return new WaitForSeconds(fadeDuration);
+        
+        //perform actions after fading in
+        ToggleObjectVisibility(targetMaterial);
+        //fadeCanvas.QuickFadeOut();
+        
+        //perform actions after fading out
+        RenderSettings.skybox = _skyboxMaterial;
+        onCompleteAction.Invoke();
+
+    }
+
+    private void ToggleObjectVisibility(Material targetMaterial) {
+        foreach (GameObject obj in objectsInScene) {
+            obj.SetActive(targetMaterial.Equals(_skyboxMaterial));
+        }
+    }
     void SetMovementActive(bool active){
         playerTransform.Find("Locomotion").Find("Move").gameObject.SetActive(active);
         playerTransform.Find("Locomotion").Find("Teleportation").gameObject.SetActive(active);
         playerTransform.Find("Locomotion").Find("Climb").gameObject.SetActive(active);
         playerTransform.Find("Locomotion").Find("Gravity").gameObject.SetActive(active);
         playerTransform.Find("Locomotion").Find("Jump").gameObject.SetActive(active);
-    }
-    
-    private IEnumerator FadeCoroutine(float startAlpha, float endAlpha, bool endState) {
-        float elapsedTime = 0f;
-        
-        while (elapsedTime < fadeDuration) {
-            elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeDuration);
-            
-            videoMaterial.SetFloat("_Alpha", alpha);
-            yield return null;
-        }
-        videoMaterial.SetFloat("_Alpha", endAlpha);
-        _isActive = endState;
-    }
-    
-    private IEnumerator WaitForFade(bool endState) {
-        // Wait until the current coroutine finishes
-        while (_isActive != endState) {
-            yield return null;
-        }
     }
 }
 
